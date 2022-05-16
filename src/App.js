b3ab8b74578156a1b1fd04bd5152c2a51c1bca49
Franc <francissteven1@gmail.com>
2022-05-16 17:20:29 +0300

    make it better

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 function App() {
   return (
-    <div className="flex flex-col bg-gray-800 text-center h-full w-full">
+    <div className="flex flex-col bg-gray-800 text-center h-screen w-full">
       <div className="w-16 h-16 mx-auto mt-24 mb-4">
         <img src="https://i.ibb.co/5WpZ62y/logo512.png" alt="logo512" />
       </div>
       <div className="flex flex-col">
         <h1 className="text-2xl text-white font-bold">Turage</h1>
-        <p className="text-md text-white">Your #1 Ride Sharing App</p>
+        <p className="text-sm text-white">Your #1 Ride Sharing App</p>
       </div>
     </div>
   );
